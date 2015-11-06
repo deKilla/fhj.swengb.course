80c7c5d81c265311528d330016a293ae1b276f25
McKorleone <paul.koerner@edu.fh-joanneum.at>
2015-11-06 13:30:59 +0100

    Buttons for calculator, added new methods

--- material/src/main/scala/fhj/swengb/lectures/l03/JavaFxHelloworld.scala
+++ material/src/main/scala/fhj/swengb/lectures/l03/JavaFxHelloworld.scala
@@ -51,4 +51,23 @@ class HelloworldController extends Initializable {
     println("clicked")
   }
 
+  def one(): Unit = print("1")
+  def two(): Unit = print("2")
+  def three(): Unit = print("3")
+  def four(): Unit = print("4")
+  def five(): Unit = print("5")
+  def six(): Unit = print("6")
+  def seven(): Unit = print("7")
+  def eight(): Unit = print("8")
+  def nine(): Unit = print("9")
+  def zero(): Unit = print("0")
+  def plus(): Unit = print("+")
+  def minus(): Unit = print("-")
+  def divide(): Unit = print("/")
+  def multiply(): Unit = print("*")
+  def percent(): Unit = print("/%")
+  def equals(): Unit = print("=")
+  def dot(): Unit = print(".")
+  def clear(): Unit = print("C")
+  def plusminus(): Unit = print("+/-")
 }
