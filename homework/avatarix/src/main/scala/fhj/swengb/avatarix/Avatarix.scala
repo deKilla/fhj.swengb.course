9c6803c019547d1bb5b3db903fcb8b6d4063e8a6
Zerberuss <an-schneider@hotmail.com>
2015-11-20 18:42:06 +0100

    krebs

--- homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
+++ homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
@@ -9,7 +9,7 @@ import javafx.scene.layout.BorderPane
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
 
-import fhj.swengb.{Students, Speakers}
+import fhj.swengb.Speakers
 
 import scala.util.control.NonFatal
 
@@ -29,7 +29,7 @@ class Avatarix extends javafx.application.Application {
 
   override def start(stage: Stage): Unit =
     try {
-      stage.setTitle("Avatarix - Gruppe1")
+      stage.setTitle("Avatarix")
       loader.load[Parent]() // side effect
       val scene = new Scene(loader.getRoot[Parent])
       stage.setScene(scene)
@@ -43,12 +43,13 @@ class Avatarix extends javafx.application.Application {
 
 
 class AvatarixController extends Initializable {
-  @FXML var seppl77: BorderPane = _
+  @FXML var borderPane: BorderPane = _
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
-    val url: String = Students.aschneider.gitHubUser.avatarUrl.toString
+    val url: String = Speakers.rladstaetter.gitHubUser.avatarUrl.toString
     //val url = Students.mfuchs.gitHubUser.avatarUrl.toString
-    //seppl77.setCenter(new ImageView(new Image(url)))
+
+    borderPane.setCenter(new ImageView(new Image(url)))
   }
 
 }
